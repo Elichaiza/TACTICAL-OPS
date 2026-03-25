92d7947f521a5863391d2c6224227005eb6a2735
elichaiza <elichaiza@gmail.com>
2026-03-25 15:13:35 +0200

    Add GitHub Actions workflow for auto-deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   esbuild: false,
+  base: '/TACTICAL-OPS/',  
   server: {
     port: 5173,
     host: true
